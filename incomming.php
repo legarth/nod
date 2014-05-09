65331c35789d85c67d6c1d32efe21c032a85d49f
OpenShift guest <legarth@gmail.com>
2014-05-09 09:56:30 -0400

    Adding forward sms code

--- incomming.php
+++ incomming.php
@@ -1,5 +1,55 @@
 <?php
-
+class SendSMS
+{
+	private $url = 'http://api.textmarketer.co.uk/services/rest/sms'; // url of the service
+	private $username = '9qqkd'; // CHANGE THIS!!!
+	private $password = 'azr90'; // CHANGE THIS!!
+	private $message_id,$credits_used;
+	function __construct()
+	{
+	}
+	public function getMessageID()
+	{
+		return $this->message_id;
+	}
+	public function getCreditsUsed()
+	{
+		return $this->credits_used;
+	}
+	// public function to commit the send
+	public function send($message,$mobile,$originator)
+	{
+		$url_array= array('message'=>$message,'mobile_number'=>$mobile, 'originator'=>$originator,
+		'username'=>$this->username, 'password'=>$this->password);
+		$url_string = $data = http_build_query($url_array, '', '&');
+		// we're using the curl library to make the request
+		$curlHandle = curl_init();
+		curl_setopt($curlHandle, CURLOPT_URL, $this->url);
+		curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curlHandle, CURLOPT_POSTFIELDS, $url_string);
+		curl_setopt($curlHandle, CURLOPT_POST, 1);
+		$responseBody = curl_exec($curlHandle);
+		$responseInfo  = curl_getinfo($curlHandle);
+		curl_close($curlHandle);
+		return $this->handleResponse($responseBody,$responseInfo);
+	}
+	private function handleResponse($body,$info)
+	{
+		if ($info['http_code']==200){ // successful submission
+			$xml_obj = simplexml_load_string($body);
+			// extract message id and credit usuage
+			$this->message_id = (int) $xml_obj->message_id;
+			$this->credits_used = (int) $xml_obj->credits_used;
+			return true;
+		}
+		else{
+			$this->message_id = null;
+			$this->credits_used = null;
+			// error handling
+			return false;
+		}
+	}
+}
 $allgood = 1;
 
 if (!empty($_GET['number']))
@@ -63,24 +113,33 @@ if($allgood)
                                                                                             $number."','".
                                                                                             $network."')";
             echo $insertstr ."<p>";
-            if (mysqli_connect_errno()) {
-              echo "Failed to connect to MySQL: " . mysqli_connect_error() . "<p>";
+  
+            echo "inserting";
+            $insertresult = mysqli_query($con, $insertstr);
+            
+            if(!$insertresult)
+            {
+               // echo "Error";
+                die('Error: ' . mysqli_error($con));
             }
             else
             {
-                echo "inserting";
-                $insertresult = mysqli_query($con, $insertstr);
+                echo "Insert done, now send to reciever<p>";
                 
-                if(!$insertresult)
-                {
-                   // echo "Error";
-                    die('Error: ' . mysqli_error($con));
-                }
-                else
-                {
-                    echo "Insert done";
-                }
+                $smstext = "Hi ".$user_result['name']." you have just recived a nod from someone. They said '".$message."'";
+                
+                
+                echo "Sending: <p>";
+                echo $smstext."<p>";
+                echo "To: <p>";
+                echo $user_result['number']."<p>";
+               /*
+                $sms = new SendSMS();
+                if($sms->send($smstext,"'".$user_result['number']."'","Nod")) echo "Yay, sent!";
+                else echo "Boo, not sent"; 
+                */
             }
+
         }
      
         
