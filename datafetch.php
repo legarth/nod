a8283e93695fbd131ed145695aee901de38f7fbe
legarth <legarth@gmail.com>
2014-09-23 14:39:43 +0000

    adding data fetch

--- datafetch.php
+++ datafetch.php
@@ -4,7 +4,7 @@
     
   function foo()
   {
-    $branchranking = "SELECT br.name, COUNT(*) as nods FROM incoming i INNER JOIN users u on i.user = u.username INNER JOIN branch br on u.branch = br.id INNER JOIN business bu on bu.id = br.businessid GROUP BY u.branch ORDER BY COUNT(*) DESC";
+    $branchranking = "SELECT br.name, COUNT(*) as nods FROM incoming i INNER JOIN users u on i.user = u.username INNER JOIN branch br on u.branch = br.id INNER JOIN business bu on bu.id = br.businessid WHERE bu.id = 1 GROUP BY u.branch ORDER BY COUNT(*) DESC";
    // echo $branchranking;
     
     $con=mysqli_connect("127.3.124.2","adminX8wp3r5","NSQp-43aGVXv","nod");
