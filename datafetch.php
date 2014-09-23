fafeeed44fda1b32f4c044bba1f87eb952bae34c
legarth <legarth@gmail.com>
2014-09-23 12:37:04 +0000

    adding data fetch

---
+++ datafetch.php
@@ -0,0 +1,22 @@
+<?php
+
+    //quries
+    $branchranking ="SELECT br.name, COUNT(*) as nods FROM incoming i INNER JOIN users u on i.user = u.username INNER JOIN branch br on u.branch = br.id
+INNER JOIN business bu on bu.id = br.businessid WHERE bu.id = 1 GROUP BY u.branch ORDER BY COUNT(*) DESC";
+  function foo()
+  {
+      
+    $con=mysqli_connect("127.3.124.2","adminX8wp3r5","NSQp-43aGVXv","nod");
+    
+    // Check connection
+    if (mysqli_connect_errno()) {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error() . "<p>";
+    }
+    else 
+    {
+        $rank_result = mysqli_query($con,$branchranking);
+         
+         echo "rows ". mysqli_num_rows($rank_result);
+    }   
+  }
+?>
--- helloworld.php
+++ helloworld.php
@@ -1,8 +1,57 @@
+<!DOCTYPE html>
+<?php include 'datafetch.php'; ?>
+<html>
+<head>
+<style>
+
+  h1   {color:white}
+  p    {color:white}
+  body {
+   font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif; 
+   font-weight: 300; background-color:#a71681;
+}
+  .nodlogo
+  {
+    position: absolute;
+    left: 35px;
+    top: 25px;
+    width: 320px;
+    height: 145px;
+  }
+  .nodheader
+  {
+    position: absolute;
+    top: 190px;
+    margin:0px 25px 0px 25px;
+  }
+  .noddata
+  {
+    position: absolute;
+    top: 240px;
+    margin:0px 25px 0px 25px;
+  }  
+</style>
+</head>
+<body>
+<img src="img/NodLogoCroppedNoLine.png" class="nodlogo">
+
+<h1 id="header" class="nodheader">
+Whitestuff Nod report
+</h1>
+<p id="data" class="noddata">
+
+
 <?php
-// A simple web site in Cloud9 that runs through Apache
-// Press the 'Run' button on the top to start the web server,
-// then click the URL that is emitted to the Output tab of the console
-
-echo 'Hello world from Cloud9! Ben and Joe are dumb and smell of cheese';
-
+foo(); // call the function
 ?>
+</p>
+
+
+</p>
+
+</body>
+</html>
+
+
+
+
